a429de67760a3d26d056bdb392e319ea0a1bf4b5
owen151128 <owen151128@gmail.com>
2017-08-04 18:13:58 +0900

    Base

---
+++ app/src/main/java/com/hpcnt/canvasstudy/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hpcnt.canvasstudy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
